33b5a0358e2b53ebf17197ee149efded0fb05839
lisvip <118431688+lisvip@users.noreply.github.com>
2026-07-04 21:48:36 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,19 @@
 
+export default async function handler(req) {
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization"
+      }
+    })
+  }
+  const url = new URL(req.url);
+  url.hostname = "api.openai.com";
+  const res = await fetch(new Request(url, req));
+  const newRes = new Response(res.body, res);
+  newRes.headers.set("Access-Control-Allow-Origin", "*");
+  return newRes;
+}
+export const config = { runtime: "edge" };
